224dada7fbf0d81ef7e4efaa89c60e46765ff73b
Victor Momodu <victoramomodu@gmail.com>
2026-01-23 16:09:50 +0100

    feat: add email sending to admin seeder

--- src/prisma/seeders/admin.seeder.ts
+++ src/prisma/seeders/admin.seeder.ts
@@ -1,7 +1,18 @@
 import { type PrismaClient, UserRole } from '@prisma/client';
+import { createClient } from 'smtpexpress';
+import { TokenPayload } from 'src/auth/auth.schema';
 import { env } from 'src/environment';
+import {
+  EmailSubject,
+  setPasswordTemplate,
+} from 'src/message-queue/message-queue.schema';
+import * as jwt from 'jsonwebtoken';
 
 export default async function seedAdmins(prisma: PrismaClient) {
+  const emailClient = createClient({
+    projectId: env.SMTPEXPRESS_PROJECT_ID,
+    projectSecret: env.SMTPEXPRESS_PROJECT_SECRET,
+  });
   console.log('\n=== Admin Seeding Started ===\n');
 
   for (const admin of env.DEFAULT_ADMINS) {
@@ -16,7 +27,7 @@ export default async function seedAdmins(prisma: PrismaClient) {
       continue;
     }
 
-    await prisma.user.create({
+    const createdUser = await prisma.user.create({
       data: {
         email: admin.email,
         role: UserRole.ADMIN,
@@ -26,7 +37,27 @@ export default async function seedAdmins(prisma: PrismaClient) {
       },
     });
 
-    // TODO: Send activation token
+    const payload: TokenPayload = {
+      sub: createdUser.id,
+      email: createdUser.email,
+      role: UserRole.ADMIN,
+    };
+    const token = jwt.sign(payload, env.JWT_SECRET, {
+      expiresIn: '7d',
+    });
+    const url = new URL(env.FRONTEND_BASE_URL + '/activate-account');
+    url.searchParams.set('token', token);
+
+    await emailClient.sendApi.sendMail({
+      subject: EmailSubject.ACTIVATE_ACCOUNT,
+      message: setPasswordTemplate(true, url.toString()),
+      sender: {
+        name: 'Obafemi Awolowo University - College of Health Sciences',
+        email: env.SMTPEXPRESS_SENDER_EMAIL,
+      },
+      recipients: [createdUser.email],
+    });
+
     console.log(`→ Created Admin: ${admin.email}\n`);
   }
 
