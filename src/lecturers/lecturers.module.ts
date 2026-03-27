6b9837e16313ffceb132d7357355fef94841e36e
Moyin-olugbenga <olugbengamoyinoluwa839@gmail.com>
2026-03-27 14:23:19 +0100

    Created result Upload per dept for lecturers

--- src/lecturers/lecturers.module.ts
+++ src/lecturers/lecturers.module.ts
@@ -5,9 +5,10 @@ import { LecturersController } from './lecturers.controller';
 import { LecturerController } from './lecturer.controller';
 import { MessageQueueModule } from 'src/message-queue/message-queue.module';
 import { ApprovalsService } from 'src/approvals/approvals.service';
+import { ApprovalModule } from 'src/approvals/approvals.module';
 
 @Module({
-  imports: [MessageQueueModule],
+  imports: [MessageQueueModule, ApprovalModule],
   controllers: [LecturersController, LecturerController],
   providers: [LecturersService, LecturerService, ApprovalsService],
 })
